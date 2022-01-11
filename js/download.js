5f47ed618bba9e1f6d449a178286f38b8ed5f683
Pigeon2333 <68182691+Pigeon2333@users.noreply.github.com>
2022-01-11 10:31:35 +0800

    Update download.js

--- js/download.js
+++ js/download.js
@@ -1,5 +1,5 @@
-var ver="20201203",
-dl_link="http://idc.cubepaas.com:46016/latest.zip"
+var ver="2022.1.11",
+dl_link="https://github.com/FillAmeaPixelNetWork/EZ4H/releases/download/v0.2/EZ4H_v0.2.zip"
 document.getElementById("ver").innerHTML=ver;
 document.getElementById("dl1").href=dl_link;
 document.getElementById("dl2").href=dl_link;
