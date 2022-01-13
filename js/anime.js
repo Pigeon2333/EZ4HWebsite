9e05275d3d82fc23a4d35ea7ad12b30359b74a81
PexEric <3088364720@qq.com>
2022-01-13 18:27:46 +0800

    简单更改

--- js/anime.js
+++ js/anime.js
@@ -1,5 +1,5 @@
-var str="一个第三方代理服务端，允许使用Java版客户端的玩家进入基岩版服务器。                ",
-tags=["现已由FillAmeaPixel团队接手.","咕咕咕？咕咕咕！"],
+var str="一个第三方Minecraft服务端代理，</br>允许通过Java版客户端进入基岩版服务器。</br></br>",
+tags=["现已由FillAmeaPixel接手。","咕咕咕？咕咕咕！"],
 element=document.getElementById("SUBTITLE"),
 stat={count:0,tag:0,stage:0};
 setInterval(function(){
