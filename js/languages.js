a7807274619c61ab626439e4590961d63209aa15
PexEric <3088364720@qq.com>
2022-01-14 13:00:26 +0800

    多语言2

---
+++ js/languages.js
@@ -0,0 +1,13 @@
+var jsSrc =(navigator.language || navigator.browserLanguage).toLowerCase();
+if(jsSrc.indexOf('zh')!=-1)
+{
+
+}
+else if(jsSrc.indexOf('en')!=-1)
+{
+    window.location.href="/en/";
+}
+else
+{
+    window.location.href="/en/";
+}
