93dfd0837c0e1725fec6e8d7b864fb1c3765db5f
PexEric <3088364720@qq.com>
2022-01-14 10:27:00 +0800

    多语言

---
+++ en/js/download.js
@@ -0,0 +1,5 @@
+var ver="2022.1.11",
+dl_link="https://github.com/FillAmeaPixelNetWork/EZ4H/releases/download/v0.2/EZ4H_v0.2.zip"
+document.getElementById("ver").innerHTML=ver;
+document.getElementById("dl1").href=dl_link;
+document.getElementById("dl2").href=dl_link;
